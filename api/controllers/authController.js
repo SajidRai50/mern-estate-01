2d17ce417c178fb5f0405a580f16cd4406e38a87
RaiSajid50 <raisajid94@gmail.com>
2026-03-06 14:25:24 +0500

    errorHandler,midlewear

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,17 +1,18 @@
 import User from "../models/userModel.js";
 import bcrypt from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
 
-export const signup =async (req,res)=>{
+export const signup =async (req,res,next)=>{
  const {username,email,password} =req.body;
  const hashedPassword = bcrypt.hashSync(password,10);
- console.log(req.body)
+ console.log(req.body,'authController req.body')
  const newUser = new User({username,email,password:hashedPassword});
  try {
   await newUser.save();
  res.status(201).json("user creater successfull")
  } catch (error) {
- res.status(500).json(error.message)
+ next(error);
  }
 
 }
