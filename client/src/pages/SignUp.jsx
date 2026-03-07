718fa58961f157791ac3e3026bf31324f678b12f
RaiSajid50 <raisajid94@gmail.com>
2026-03-07 11:20:00 +0500

    modified:   client/src/pages/SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 const SignUp = () => {
+const[formData ,setFormData]= useState({});
+const handleChange =(e)=>{
+ setFormData({
+  ...formData,
+  [e.target.value]:e.target.value,
+ });
+}
+
+const handleSubmit =(e) =>{
+  e.preventDefault();
+}
+console.log(formData)
+ 
   return (
     <div>
        <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-            <form  className="flex flex-col gap-4">
+            <form onSubmit={handleSubmit}
+             className="flex flex-col gap-4">
 
-              <input
+              <input onChange={handleChange}
                 type="text"
                 placeholder="username"
                 className="border p-3 rounded-lg"
                 id="username"
 
               />
-              <input
+              <input onChange={handleChange}
                 type="email"
                 placeholder="email"
                 className="border p-3 rounded-lg"
                 id="email"
 
               />
-              <input
+              <input onChange={handleChange}
                 type="password"
                 placeholder="password"
                 className="border p-3 rounded-lg"
@@ -38,7 +53,7 @@ const SignUp = () => {
 
             </form>
 
-            
+
             <div className="flex gap-2 mt-5">
               <p>Dont have an account?</p>
               <Link to={"/sign-up"}>
